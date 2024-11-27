dad9908384b2f25cb57e5934ed0c32f62c453d90
Artemii <artemijnovokresenov548@gmail.com>
2024-11-27 19:46:46 +0500

    новая папка для С#

---
+++ Новая папка/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Код;
+
+class Program
+{
+    static void Main()
+    {
+      int[] numbers = new int [5];
+      string[] strings = ("string1", "string2", "string3");
+
+      int[] numbers2 = (1, 2, 3);
+      Console.WriteLine(numbers2[1]);
+      numbers2[2] += 100;
+      Console.WriteLine(numbers2.Length);
+
+      int[] numbers3 = new int [4];
+      int[] numbers4 = new int [5];
+      char[] chars = new char[3];
+      char[] chars2 = ("1", "2", "3", "4", "5");
+    }
+}
