dc9bc2d4e2347258a3b68190cbbc75a1ccf02031
Artemii <artemijnovokresenov548@gmail.com>
2024-12-16 19:49:03 +0500

    Транспонирование матрицы

--- Новая папка/Program.cs
+++ Новая папка/Program.cs
@@ -1,20 +1,53 @@
-﻿namespace Код;
+﻿namespace Kodik;
 
-class Program
+class Matrix
 {
     static void Main()
     {
-      int[] numbers = new int [5];
-      string[] strings = ("string1", "string2", "string3");
-
-      int[] numbers2 = (1, 2, 3);
-      Console.WriteLine(numbers2[1]);
-      numbers2[2] += 100;
-      Console.WriteLine(numbers2.Length);
-
-      int[] numbers3 = new int [4];
-      int[] numbers4 = new int [5];
-      char[] chars = new char[3];
-      char[] chars2 = ("1", "2", "3", "4", "5");
+        Console.WriteLine("Введите количество NxM элементов матрицы sample: ");
+        int N = 0;
+        int M = 0;
+        
+        string str = Console.ReadLine();
+        string stl = Console.ReadLine();
+        N = int.Parse(str);
+        M = int.Parse(stl);
+        
+ 
+        Console.WriteLine("Исходная матрица: ");
+        Console.WriteLine();
+        int[,] sample = new int[N, M];
+        Random rnd = new Random();
+ 
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                sample[i, j] = rnd.Next(1, 100); // заполнение матрицы случайными числами
+ 
+                Console.Write(sample[i, j] + " \t ");
+ 
+                if (j == M - 1)
+                    Console.WriteLine();
+            }
+        }
+        int[,] trans = new int[M, N];
+        Console.WriteLine();
+        Console.WriteLine("Транспонированная матрица: ");
+        Console.WriteLine();
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                trans[i, j] = sample[j, i];
+                Console.Write(trans[i, j] + " \t ");
+ 
+                if (j == M - 1)
+                    Console.WriteLine();
+            }
+        }
+ 
+        Console.ReadLine();
+ 
     }
 }
